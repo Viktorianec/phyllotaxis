b78cff58abd29ad1bb7151b7d3ac6e794176c5d1
Viktorianec <viktorianecc@gmail.com>
2022-08-23 21:04:46 +0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Viktorianec on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
